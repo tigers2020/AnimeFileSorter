ece617a66e0b8d09ff32f262e089837213691b43
AnimeFileSorter Dev <anime.sorter@example.com>
2025-05-06 20:24:15 -0400

    초기화 및 개발 변경으로

--- src/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-AnimeFileSorter의 컨트롤러 패키지입니다.
-이 패키지는 모델과 뷰 간의 상호작용을 관리하는 컨트롤러 코드를 포함합니다.
-""" 
--- src/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-AnimeFileSorter의 유틸리티 패키지입니다.
-이 패키지는 여러 모듈에서 사용되는 도우미 함수와 유틸리티를 포함합니다.
-""" 
